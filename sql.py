782e547de01515f4c849c0da6094e8995a36d257
Christian Schudoma <schudoma@mpimp-golm.mpg.de>
2012-01-17 18:09:59 +0100

    Fixed SQL-NULL-value issue.

--- sql.py
+++ sql.py
@@ -16,6 +16,17 @@ def write_sql_header(db_name, table_name, table, out=sys.stdout):
                                         ',\n'.join(table))))
     pass
 
+def format_entry(entry):
+    """ This is a really really ugly workaround... """
+    formatted = []
+    for x in entry:
+        if isinstance(x, str) and x != 'NULL':
+            formatted.append("'%s'" % x)
+        else:
+            formatted.append(x)
+    return '(%s)' % ','.join(map(str, formatted))
+    
+
 def write_sql_table(data, columns_d, table_name='DUMMY', out=sys.stdout, index=0):
     for dobj in data:
         entry = []        
@@ -24,12 +35,15 @@ def write_sql_table(data, columns_d, table_name='DUMMY', out=sys.stdout, index=0
                 entry.append(val + (getattr(dobj, key),))
             else:
                 entry.append(val[:-1] + (str, 'NULL'))
+            pass
+
         entry = [(-1, 'id', int, index)] + entry
         index += 1
+
         try:
             out.write(INSERT_STR % (table_name,
-                                    tuple([x[2](x[3]) 
-                                           for x in sorted(entry)])))
+                                    format_entry([x[2](x[3]) 
+                                                  for x in sorted(entry)])))
         except:
             sys.stderr.write('EXC: %s\n' % sorted(entry))
             sys.exit(1)
